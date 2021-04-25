377407b8446b2cd64ee74e06c06c0bdbe5919ce3
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-25 10:24:23 -0300

    feat(app): start first architecture

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('dotenv/config');
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get('*', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || '8080');
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
