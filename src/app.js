f59d1ab6c3ad7ef1e7fd431ae36a1fe118df99cc
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-27 19:28:52 -0300

    feat(helmet): add helmet dependency

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 require('dotenv/config');
+const { Article } = require('./database/models');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('Hello World!');
+app.use(helmet());
+app.use(bodyParser.json());
+app.get('*', async (req, res) => {
+  const articles = await Article.query()
+    .withGraphFetched('author')
+    .omit(['id', 'author_id']);
+
+  return res.json(articles);
 });
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
