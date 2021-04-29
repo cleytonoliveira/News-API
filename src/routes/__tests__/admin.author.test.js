5cbb6bb788b289641273b26a0fe31c538b839c77
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-29 18:51:33 -0300

    test(authors): implementation endpoint GET:/admin/authors

--- src/routes/__tests__/admin.author.test.js
+++ src/routes/__tests__/admin.author.test.js
@@ -15,59 +15,85 @@ const user = {
   password: '12345678',
 };
 
-describe('POST :/api/admin/authors', () => {
-  beforeEach(() => {
-    shell.exec('npx knex migrate:rollback');
-    shell.exec('npx knex migrate:latest');
-    shell.exec('npx knex seed:run');
+describe('Authors', () => {
+  describe('POST :/api/admin/authors', () => {
+    beforeEach(() => {
+      shell.exec('npx knex migrate:rollback');
+      shell.exec('npx knex migrate:latest');
+      shell.exec('npx knex seed:run');
+    });
+
+    it('should be able to register a new author with successful', async () => {
+      const { body: { token } } = await request.post('/api/login')
+        .send({
+          email: user.email,
+          password: user.password,
+        })
+        .expect(200);
+
+      const response = await request.post('/api/admin/authors')
+        .send(newAuthor)
+        .set('Authorization', `${token}`);
+
+      expect(response.statusCode).toEqual(201);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body).toHaveProperty('name', newAuthor.name);
+      expect(response.body).toHaveProperty('picture', newAuthor.picture);
+    });
+
+    it('shouldn\'t be able to register a new author if user is not admin', async () => {
+      const { body: { token } } = await request.post('/api/login')
+        .send({
+          email: 'first_author@mail.com',
+          password: '12345678',
+        });
+
+      const response = await request.post('/api/admin/authors')
+        .send(newAuthor)
+        .set('Authorization', `${token}`);
+
+      expect(response.statusCode).toEqual(401);
+      expect(response.body.message).toBe('Only administrator can access');
+    });
+
+    it('shouldn\'t be able to register a new author without token', async () => {
+      const response = await request.post('/api/admin/authors')
+        .send(newAuthor);
+
+      expect(response.statusCode).toEqual(401);
+      expect(response.body.message).toBe('Token not found');
+    });
+
+    it('shouldn\'t be able to register a new author with invalid token', async () => {
+      const response = await request.post('/api/admin/authors')
+        .send(newAuthor)
+        .set('Authorization', '9999999999999');
+
+      expect(response.statusCode).toEqual(401);
+      expect(response.body.message).toBe('Invalid or expired token');
+    });
   });
 
-  it('should be able to register a new author with successful', async () => {
-    const { body: { token } } = await request.post('/api/login')
-      .send({
-        email: user.email,
-        password: user.password,
-      });
+  describe('GET :/api/admin/authors', () => {
+    beforeEach(() => {
+      shell.exec('npx knex migrate:rollback');
+      shell.exec('npx knex migrate:latest');
+      shell.exec('npx knex seed:run');
+    });
 
-    const response = await request.post('/api/admin/authors')
-      .send(newAuthor)
-      .set('Authorization', `${token}`);
+    it('should be able to get all authors', async () => {
+      const { body: { token } } = await request.post('/api/login')
+        .send({
+          email: user.email,
+          password: user.password,
+        })
+        .expect(200);
 
-    expect(response.statusCode).toEqual(201);
-    expect(response.body).toHaveProperty('id');
-    expect(response.body).toHaveProperty('name', newAuthor.name);
-    expect(response.body).toHaveProperty('picture', newAuthor.picture);
-  });
+      const response = await request.get('/api/admin/authors')
+        .send(newAuthor)
+        .set('Authorization', `${token}`);
 
-  it('shouldn\'t be able to register a new author if user is not admin', async () => {
-    const { body: { token } } = await request.post('/api/login')
-      .send({
-        email: 'first_author@mail.com',
-        password: '12345678',
-      });
-
-    const response = await request.post('/api/admin/authors')
-      .send(newAuthor)
-      .set('Authorization', `${token}`);
-
-    expect(response.statusCode).toEqual(401);
-    expect(response.body.message).toBe('Only administrator can access');
-  });
-
-  it('shouldn\'t be able to register a new author without token', async () => {
-    const response = await request.post('/api/admin/authors')
-      .send(newAuthor);
-
-    expect(response.statusCode).toEqual(401);
-    expect(response.body.message).toBe('Token not found');
-  });
-
-  it('shouldn\'t be able to register a new author with invalid token', async () => {
-    const response = await request.post('/api/admin/authors')
-      .send(newAuthor)
-      .set('Authorization', '9999999999999');
-
-    expect(response.statusCode).toEqual(401);
-    expect(response.body.message).toBe('Invalid or expired token');
+      expect(response.statusCode).toEqual(200);
+    });
   });
 });
