099cf33f9ac7c83e10749842f8622da7f105c0af
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-29 00:16:23 -0300

    fix(test): adjust some implementation of test

--- src/routes/__tests__/user.test.js
+++ src/routes/__tests__/user.test.js
@@ -5,6 +5,13 @@ const app = require('../../app');
 
 const request = supertest(app);
 
+const user = {
+  name: 'New User',
+  email: 'newuser@email.com',
+  password: '12345678',
+  picture: 'https://i.pravatar.cc/150',
+};
+
 describe('POST :/api/sign-up', () => {
   beforeEach(() => {
     shell.exec('npx knex migrate:rollback');
@@ -14,12 +21,7 @@ describe('POST :/api/sign-up', () => {
 
   it('should be able to register a new user with successful', async () => {
     const response = await request.post('/api/sign-up')
-      .send({
-        name: 'New User',
-        email: 'newuser@email.com',
-        password: '12345678',
-        picture: 'https://i.pravatar.cc/150',
-      });
+      .send(user);
 
     expect(response.statusCode).toEqual(201);
     expect(response.body).toHaveProperty('token');
@@ -31,7 +33,10 @@ describe('POST :/api/sign-up', () => {
   it('shouldn\'t be able to register with invalid name', async () => {
     const response = await request.post('/api/sign-up')
       .send({
-
+        name: 'Teste',
+        email: user.email,
+        password: user.password,
+        picture: user.picture,
       });
 
     expect(response.statusCode).toEqual(400);
@@ -41,7 +46,10 @@ describe('POST :/api/sign-up', () => {
   it('shouldn\'t be able to register with invalid email', async () => {
     const response = await request.post('/api/sign-up')
       .send({
-
+        name: user.name,
+        email: 'mail@3.com',
+        password: user.password,
+        picture: user.picture,
       });
 
     expect(response.statusCode).toEqual(400);
@@ -51,7 +59,10 @@ describe('POST :/api/sign-up', () => {
   it('shouldn\'t be able to register with invalid password', async () => {
     const response = await request.post('/api/sign-up')
       .send({
-
+        name: user.name,
+        email: user.email,
+        password: '1234',
+        picture: user.picture,
       });
 
     expect(response.statusCode).toEqual(400);
@@ -60,21 +71,11 @@ describe('POST :/api/sign-up', () => {
 
   it('shouldn\'t be able to register with email already registered in database', async () => {
     await request.post('/api/sign-up')
-      .send({
-        name: 'New User',
-        email: 'newuser@email.com',
-        password: '12345678',
-        picture: 'https://i.pravatar.cc/150',
-      })
+      .send(user)
       .expect(201);
 
     const response = await request.post('/api/sign-up')
-      .send({
-        name: 'New User',
-        email: 'newuser@email.com',
-        password: '12345678',
-        picture: 'https://i.pravatar.cc/150',
-      });
+      .send(user);
 
     expect(response.statusCode).toEqual(409);
     expect(response.body.message).toBe('Email already registered.');
