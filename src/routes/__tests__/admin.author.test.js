43d1027e1952a555693b69d175bea3419bf457e9
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-29 15:43:38 -0300

    test(admin/authors): test to register new author

---
+++ src/routes/__tests__/admin.author.test.js
@@ -0,0 +1,45 @@
+const supertest = require('supertest');
+const shell = require('shelljs');
+
+const app = require('../../app');
+
+const request = supertest(app);
+
+const newAuthor = {
+  name: 'New User',
+  picture: 'https://i.pravatar.cc/150',
+};
+
+const user = {
+  email: 'adm@news.com',
+  password: '12345678',
+};
+
+describe('POST :/api/admin/authors', () => {
+  beforeEach(() => {
+    shell.exec('npx knex migrate:rollback');
+    shell.exec('npx knex migrate:latest');
+    shell.exec('npx knex seed:run');
+  });
+
+  it('should be able to register a new author with successful', async () => {
+    const { body: { token } } = await request.post('/api/login')
+      .send({
+        email: user.email,
+        password: user.password,
+      });
+
+    const response = await request.post('/api/admin/authors')
+      .send(newAuthor)
+      .set('Authorization', `${token}`);
+
+    expect(response.statusCode).toEqual(201);
+  });
+
+  it('shouldn\'t be able to register a new author without token', async () => {
+    const response = await request.post('/api/admin/authors')
+      .send(newAuthor);
+
+    expect(response.statusCode).toEqual(401);
+  });
+});
