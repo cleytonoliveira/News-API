86e5ea7588df9713f11870bba70d9777082a8aeb
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-05-14 00:10:32 -0300

    fix: correct seed to hash

--- src/database/seeds/01_users.js
+++ src/database/seeds/01_users.js
@@ -1,19 +1,24 @@
-exports.seed = (knex) => knex('users')
-  .del()
-  .then(() => knex('users')
-    .insert([
-      {
-        name: 'Administrator News',
-        email: 'adm@news.com',
-        password: '12345678',
-        role: 'admin',
-        picture: 'https://i.pravatar.cc/150',
-      },
-      {
-        name: 'User Example',
-        email: 'user@mail.com',
-        password: '12345678',
-        role: 'user',
-        picture: 'https://i.pravatar.cc/150',
-      },
-    ]));
+const { generateHash } = require('../../utils');
+
+exports.seed = async (knex) => {
+  const hashedPassword = await generateHash('12345678');
+  knex('users')
+    .del()
+    .then(() => knex('users')
+      .insert([
+        {
+          name: 'Administrator News',
+          email: 'adm@news.com',
+          password: hashedPassword,
+          role: 'admin',
+          picture: 'https://i.pravatar.cc/150',
+        },
+        {
+          name: 'User Example',
+          email: 'user@mail.com',
+          password: hashedPassword,
+          role: 'user',
+          picture: 'https://i.pravatar.cc/150',
+        },
+      ]));
+};
